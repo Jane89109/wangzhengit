e7f28e1a7598b2b9b5c8869772435b422f591c8b
Administrator <369073048@qq.com>
2015-12-30 16:40:16 +0800

    commit branch

--- Wangtest/src/com/zhonghangxin/factory2/hello.java
+++ Wangtest/src/com/zhonghangxin/factory2/hello.java
@@ -9,6 +9,7 @@ package com.zhonghangxin.factory2;
 	public static void main(String[] args) {
             
 		System.out.println("this is commit test !!");
+		System.out.println("this is branch test !!");
 	}
 
 }
